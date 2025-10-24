3b77331b23b690db9a1f574cf072dff241d2aa60
gunes1hakan <gunes1.hakan1@gmail.com>
2025-10-25 00:09:14 +0300

    Mermilerin ufoya çarpması sağlandı

--- src/uzay_oyunu_/Oyun.java
+++ src/uzay_oyunu_/Oyun.java
@@ -2,6 +2,7 @@ package uzay_oyunu_;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -10,11 +11,14 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.imageio.ImageIO;
 import javax.imageio.stream.FileImageInputStream;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+
 
 
 
@@ -56,13 +60,40 @@ public class Oyun extends JPanel implements KeyListener,ActionListener{
 	private int harcananMermi=0;			
 	private BufferedImage imageUzayGemisi;		
 	private BufferedImage imageUzayliUfo;	
-	private BufferedImage imageBulletPlasmaCyan;		//mermi fotoğrafının değişkeni
+	private BufferedImage imageBulletPlasmaCyan;		//mermi fotoğrafının değişkeni	
 	private ArrayList<Mermi> mermiler=new ArrayList<>();			//uzay gemisinden çıkan mermileri tutacak olan list
 	private int mermidirY=1;				//merminin piksel cinsinden hareket hızı
 	private int uzayliGemisiX=0;					//uzayliGemisini koordinatı
 	private int uzayliGemisidirX=2;				//uzayliGemisinin piksel cinsinden hareket hızı
 	private int uzayGemisiX=0;			//uzay Gemisinin x konumunu belirtiyoruz.
 	private int dirUzayX=20;			//uzay gemisinin piksel cinsinden hareket hızı
+	
+	public boolean kontrolEt() {		//Merminin uzayliUfosuna değip değmediği kontrolu yapılmaktadır.
+		int mermiGenislik=imageBulletPlasmaCyan.getWidth()/10;   
+		int mermiYukseklik=imageBulletPlasmaCyan.getHeight()/10;  
+		int ufoGenislik=imageUzayliUfo.getWidth()/10;          
+		int ufoYukseklik=imageUzayliUfo.getHeight()/10;         
+		
+		int daraltilmismermiGenislik=(int)(mermiGenislik*0.85);			//yukarıda yazan değişkenleri direkt kullanırsam mermi ufonun yakınından geçerken mermi ufoya değmiş gibi oyun sonlanıyor,aynı şekilde Ufo'nun ayakları arasına geldiği zaman tam ufoya isabet etmeden	
+		int daraltilmismermiYukseklik=(int)(mermiYukseklik*0.85);		//ufoya isabet etmiş gibi oyun sonlanıyor bu sorunları çözmek için soldaki gibi mermi ve ufo'nun çarpışma esnasında hesaplanan en-boy piksellerini daraltarak göze daha iyi gelen bir çarpışma görseli
+		int daraltilmisufoGenislik=(int)(ufoGenislik*0.6);				//elde ediyoruz
+		int daraltilmisufoYukseklik=(int)(ufoYukseklik*0.6);			
+																		
+		int xkenarindanmermiBoslugu=(mermiGenislik-daraltilmismermiGenislik)/2;    //soldaki değişkenler daraltılmış yeni piksellerin ortalandığında eski piksel boyutlarına göre ne kadar kaydığını gösterir.Böylece aşağıda piksel kesişimini hesaplayan if bloğunda eski
+		int ykenarindanmermiBoslugu=(mermiYukseklik-daraltilmismermiYukseklik)/2;  //pikselin sağ üst köşesine bu değerleri ekleyerek yapılan hesaplamanın artık yukarıda değerleri hesaplanmış daraltılmış piksellere göre yapılması sağlanır.
+		int xkenarindanUfoBoslugu=(ufoGenislik-daraltilmisufoGenislik)/2;			
+		int ykenarindanUfoBoslugu=(ufoYukseklik-daraltilmisufoYukseklik)/2;
+		
+		
+		for(Mermi mermi:mermiler) {		
+			//aşağıda cisimlerin birbirlerine isabet ettiğini iki cismin piksellerinin kesişimine göre hesaplıyoruz. 
+			if(new Rectangle(mermi.getX()+xkenarindanmermiBoslugu,mermi.getY()+ykenarindanmermiBoslugu,daraltilmismermiGenislik,daraltilmismermiYukseklik).intersects(new Rectangle(uzayliGemisiX+xkenarindanUfoBoslugu,0+ykenarindanUfoBoslugu,daraltilmisufoGenislik,daraltilmisufoYukseklik))) {		
+										
+				return true;			
+			}							
+		}								
+		return false;
+	}
 	
 	
 
@@ -85,12 +116,15 @@ public class Oyun extends JPanel implements KeyListener,ActionListener{
 	public void paint(Graphics g) {
 		super.paint(g);
 		
-		for(Mermi mermi:mermiler) {			//ekranın dışına doğru çıkan ateşleri arrayList yapısından çıkarttık.
-			if(mermi.getY()<0) {
-				mermiler.remove(mermi);
-			}
+		Iterator<Mermi> it=mermiler.iterator();			//ekranın dışına doğru çıkan ateşleri arrayList yapısından çıkarttık.	
+		while (it.hasNext()){								//Önceki mermi kontrolunda java,ArrayList yapısında hem gezinmeye çalışıyorsun hem de veri silmeye çalışıyorsun bu durumda herhangi bir veri çakışması olabilir deyip hata fırlatıyordu,bu hatadan kurtulmak için hem dolaşma işlemini hem  
+		    Mermi i=it.next();							//de veri silme işlemini aynı anda yapabildiğimiz için Iterator interface'ini kullandık.
+		    if(i.getY()<0) {
+		        it.remove();   
+		    }
 		}
-		
+
+
 		g.drawImage(imageUzayGemisi,uzayGemisiX, 490, imageUzayGemisi.getWidth()/10, imageUzayGemisi.getHeight()/10,this);	    
 		
 		g.drawImage(imageUzayliUfo,uzayliGemisiX, 0, imageUzayliUfo.getWidth()/10, imageUzayliUfo.getHeight()/10,this);
@@ -99,13 +133,22 @@ public class Oyun extends JPanel implements KeyListener,ActionListener{
 			g.drawImage(imageBulletPlasmaCyan,mermi.getX(), mermi.getY(), imageBulletPlasmaCyan.getWidth()/10, imageBulletPlasmaCyan.getHeight()/10,this);
 			
 		}
+		
+		if(kontrolEt()) {				//çarpışma olduğu zaman mesaj bastırıp oyunu sonlandırıyoruz.											
+			timer.stop();														
+			String message="Kazandınız...\n"+
+						   "harcanan mermi: "+harcananMermi+
+						   "\ngeçen süre: "+gecenSure/1000.0+" s";
+			JOptionPane.showMessageDialog(this, message);
+			System.exit(0);
+		}
 	}
 	
 	
 
 	@Override
 	public void repaint() {						
-		
+												
 		super.repaint();						
 	}
 
@@ -116,13 +159,15 @@ public class Oyun extends JPanel implements KeyListener,ActionListener{
 			mermi.setY(mermi.getY()-mermidirY);
 		}
 		
-		uzayliGemisiX+=uzayliGemisidirX;				
-		if(uzayliGemisiX>=709) {				//uzayli ufosunun sınırlara geldiği zaman yönünün değiştirilmesi sağlanıyor.
+		uzayliGemisiX+=uzayliGemisidirX;				//uzayli ufosunun sınırlara geldiği zaman yönünün değiştirilmesi sağlanıyor.
+		if(uzayliGemisiX>=709) {				
 			uzayliGemisidirX=-uzayliGemisidirX;		
 		}
 		if(uzayliGemisiX<=0) {
 			uzayliGemisidirX=-uzayliGemisidirX;
 		}
+
+		gecenSure+=5;
 		repaint();
 		
 	}
@@ -137,7 +182,7 @@ public class Oyun extends JPanel implements KeyListener,ActionListener{
 	public void keyPressed(KeyEvent e) {
 		int c=e.getKeyCode();			
 		
-		if(c==KeyEvent.VK_LEFT)	{		//yön tuşları ile uzay gemimizin hareket ettiyoruz.
+		if(c==KeyEvent.VK_LEFT)	{		//yön tuşları ile uzay gemimizi hareket ettiyoruz.
 			if(uzayGemisiX<=0) {		
 				uzayGemisiX=0;
 				return;
@@ -145,7 +190,7 @@ public class Oyun extends JPanel implements KeyListener,ActionListener{
 			else {
 				uzayGemisiX-=dirUzayX;	
 			}
-			if(uzayGemisiX<10) {	//Bu kontrolu yapmamızın sebebi,uzay gemimiz tam 0. piksele yaklaşmadan bir önceki adımda uzay gemimizin bir kısmı ekran dışına taşıyor,bu hatayı bu kontrolle çözüyoruz.
+			if(uzayGemisiX<10) {		//Bu kontrolu yapmamızın sebebi,uzay gemimiz tam 0. piksele yaklaşmadan bir önceki adımda uzay gemimizin bir kısmı ekran dışına taşıyor,bu hatayı bu kontrolle çözüyoruz.
 				uzayGemisiX=0;
 			}
 			
@@ -162,7 +207,7 @@ public class Oyun extends JPanel implements KeyListener,ActionListener{
 				uzayGemisiX=742;
 			}
 		}
-		else if(c==KeyEvent.VK_SPACE) {		//space tuşu ile yeni bir mermi oluşturduk
+		else if(c==KeyEvent.VK_SPACE) {			//space tuşu ile yeni bir mermi oluşturduk
 			mermiler.add(new Mermi(uzayGemisiX+15,470));		
 			harcananMermi++;
 		}
@@ -179,6 +224,8 @@ public class Oyun extends JPanel implements KeyListener,ActionListener{
 
 	
 }
+
+
 
 
 
